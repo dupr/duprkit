29f3f58ff2343c3406dd888c5a93b8336609d062
Mo Zhou <cdluminate@gmail.com>
2019-04-09 15:22:12 +0000

    lib: overhaul + test

--- lib/test.sh
+++ lib/test.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 set -e
-
 . ./duprkit
+export DK_VERBOSE=1
+
+src="example"
+version="0"
+maintainer="anonymous <anon@example.org>"
+dkGen_debian_changelog .
+test -r debian/changelog
+
+section="science"
+stdver="4.3.0"
+compat="11"
+description="hello world"
+dkGen_debian_control .
+test -r debian/control
 
 license="Expat"
-dkGen_debian_copyright
-test -r debian/copyright
+dkGen_debian_copyright .
+test -r debian/copyright 
+
+dkGen_debian_rules .
+test -x debian/rules
+
+dkGen_debian_source_format .
+test -r debian/source/format
